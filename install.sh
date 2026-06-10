2d90b310bfa72448642d70a972414e78a6e84692
Sergey Maksimov <m6v@mail.ru>
2026-06-10 12:37:23 +0300

    Fonts dir added

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ sudo wget -qO - https://github.com/neovim/neovim/releases/latest/download/nvim-l
 sudo ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
 git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable ~/.local/share/nvim/lazy/lazy.nvim
 
+# Шрифты можно скачать с https://github.com/ryanoasis/nerd-fonts/releases
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/UbuntuMono.zip
+# Распакуйте архив и переместите файлы .ttf или .otf в директорию ~/.local/share/fonts/ (создайте папку, если её нет)
+# Обновите кэш шрифтов в системе командой в терминале: fc-cache -f -v
